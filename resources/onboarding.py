53362c96fe47e5ed947b650d55fa8f39cc30f83f
Chiranjeevi <chiranjeevivc@gmail.com>
2019-07-16 20:34:00 -0400

    updated file

--- resources/onboarding.py
+++ resources/onboarding.py
@@ -1,7 +1,22 @@
 import sys, getopt
 
+ProjectName = sys.argv[1]
+Username = sys.argv[2]
+Password = sys.argv[3]
+
 def my_function(argv):
- print("My First Project:", argv )
- 
-if __name__ == "__main__":\
- my_function(sys.argv[1])
+   print("My First Project:", argv )
+   print(ProjectName)
+   print(Username)
+   print(Password)
+
+def createBitBucketProject():
+   print("Creating Bit Bucket Project")
+
+def createJiraProject():
+   print("Creating Jira Project")
+
+if __name__ == "__main__":
+   my_function(sys.argv[1])
+   createBitBucketProject()
+   createJiraProject()
